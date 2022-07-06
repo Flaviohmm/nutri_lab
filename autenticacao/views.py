12beae67c6c8536f3835f600fa3dada0cdffdf64
Flavio Macedo <fhenrique609@gmail.com>
2022-07-06 15:44:43 -0300

    Criando a página de cadastro

---
+++ autenticacao/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        return HttpResponse(confirmar_senha)
+
+def logar(request):
+    pass
