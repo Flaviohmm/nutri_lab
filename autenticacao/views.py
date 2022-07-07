6564ea86b2b0108341e649c6cc8a3d330b85b75b
Flavio Macedo <fhenrique609@gmail.com>
2022-07-06 23:51:03 -0300

    Ativação do Token

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,13 +1,19 @@
+import os
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
-from .utils import password_is_valid
+from django.shortcuts import get_object_or_404, redirect, render
+from .utils import password_is_valid, email_html
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from django.contrib.messages import constants
+from django.conf import settings
+from .models import Ativacao
+from hashlib import sha256
 
 
 def cadastro(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == "POST":
         username = request.POST.get('usuario')
@@ -19,15 +25,26 @@ def cadastro(request):
             return redirect('/auth/cadastro')
 
         try:
-            user = User.objects.create_user(username=username, password=senha, email=email, is_active=False)
+            user = User.objects.create_user(username=username, password=senha, is_active=False)
+    
             user.save()
-            return redirect('/auth/login')
+
+            token = sha256(f"{username}{email}".encode()).hexdigest()
+            ativacao = Ativacao(token=token, user=user)
+            ativacao.save()
+
+            path_template = os.path.join(settings.BASE_DIR, 'autenticacao/templates/emails/cadastro_confirmado.html')
+            email_html(path_template, 'Cadastro confirmado', [email,], username=username, link_ativacao=f"127.0.0.1:8000/auth/ativar_conta/{token}")
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso')
+            return redirect('/auth/logar')
         except:
             return redirect('/auth/cadastro')
 
 
 def logar(request):
     if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'logar.html')
     elif request.method == "POST":
         username = request.POST.get('usuario')
@@ -45,4 +62,18 @@ def logar(request):
 
 def sair(request):
     auth.logout(request)
+    return redirect('/auth/logar')
+
+
+def ativar_conta(request, token):
+    token = get_object_or_404(Ativacao, token=token)
+    if token.ativo:
+        messages.add_message(request, constants.WARNING, 'Esse token já foi usado')
+        return redirect('/auth/logar')
+    user = User.objects.get(username=token.user.username)
+    user.is_active = True
+    user.save()
+    token.ativo = True
+    token.save()
+    messages.add_message(request, constants.SUCCESS, 'Conta ativa com sucesso')
     return redirect('/auth/logar')
